5a4b2d278f64aa1174b77b49a92a59bd63eac7bf
MichalPt <michalptacek@outlook.com>
2025-05-31 16:00:14 +0200

    Image resolution optimized

--- src/components/carousel/EmblaCarousel.jsx
+++ src/components/carousel/EmblaCarousel.jsx
@@ -40,7 +40,7 @@ const EmblaCarousel = (props) => {
 
           {/* First slide with welcome message */}
           <div className="embla__slide text-center flex flex-col items-center justify-center">
-            <img className="embla__slide__img mb-4  dark:brightness-200 w-full h-auto object-cover" src="images/fmo-1000.png" alt="fmo"/>
+            <img className="embla__slide__img mb-4  dark:brightness-200 w-full h-auto object-cover" src="images/fmo-500.png" alt="fmo"/>
             <span className="title text-2xl sm:text-3xl bg-gradient-to-r from-accent-two/85 via-accent-one/85 to-accent-two/85 dark:from-accent-two dark:via-accent-one dark:to-accent-two bg-clip-text text-transparent">
               Welcome to MaMa Group website
             </span>
